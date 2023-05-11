580bc97ebfb1fe166a94d3239d7b504f6cf3e391
Pedro Henrique <pedro.henrique59@outlook.com>
2023-05-11 15:57:33 -0300

    Adicionando loginService e arquivo de constantes.

---
+++ src/app/service/login-service.service.ts
@@ -0,0 +1,27 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {AppConstants} from '../app-constants';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginServiceService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  login(usuario) {
+    return this.http.post(AppConstants.baseLogin, JSON.stringify(usuario)).subscribe(data => {
+        /*Retorno HTTP*/
+
+        var token = (JSON.parse(JSON.stringify(data)).Authorization.split(' ')[1]);
+
+        localStorage.setItem('token', token);
+      },
+      error => {
+        console.error('Erro ao gerar o token de login');
+        alert('Acesso Negado');
+      });
+  }
+
+}
