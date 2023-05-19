fae5b58227e54afa91e3635998c2ac68eceae84a
Pedro Henrique <pedro.henrique59@outlook.com>
2023-05-19 13:42:00 -0300

    Ajustando interceptor.

--- src/app/service/header-interceptor.service.ts
+++ src/app/service/header-interceptor.service.ts
@@ -1,6 +1,15 @@
 import {Injectable, NgModule} from '@angular/core';
-import {HTTP_INTERCEPTORS, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import {
+  HTTP_INTERCEPTORS,
+  HttpErrorResponse,
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+  HttpResponse
+} from '@angular/common/http';
+import {Observable, throwError} from 'rxjs';
+import {catchError, tap} from 'rxjs/operators';
 
 @Injectable()
 export class HeaderInterceptorService implements HttpInterceptor {
@@ -16,12 +25,29 @@ export class HeaderInterceptorService implements HttpInterceptor {
         headers: req.headers.set('Authorization', token)
       });
 
-      return next.handle(tokenRequest);
+      return next.handle(tokenRequest).pipe(
+        tap((event: HttpEvent<any>) => {
+          if (event instanceof HttpResponse && (event.status === 200 || event.status === 201)) {
+            console.info('Sucesso na operação!');
+          }
+        }), catchError(this.processaError));
     } else {
-      return next.handle(req);
+      return next.handle(req).pipe(catchError(this.processaError));
     }
-
   }
+
+  processaError(error: HttpErrorResponse) {
+    let errorMessage = 'Erro desconhecido';
+    if (error.error instanceof ErrorEvent) {
+      console.error(error.error);
+      errorMessage = 'Error: ' + error.error.error;
+    } else {
+      errorMessage = 'Código: ' + error.error.code + '\n Mensagem: ' + error.error.error;
+    }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
+  }
+
 }
 
 @NgModule({
